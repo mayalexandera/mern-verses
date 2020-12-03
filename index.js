4ea6e8806a8f195441ab685a60d977c481a40b77
mayalexandera <maya.blevins@gmail.com>
2020-12-03 18:30:39 -0500

    config mongoDB/mongoose

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+require('./services/passport')
+
+mongoose.connect(keys.mongoURI)
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken", accessToken);
-      console.log("refreshToken", refreshToken);
-      console.log("profile", profile);
-    }
-  )
-);
+// authRoutes.js exports a module object that returns a function, so this
+// registers in JS as the anonymous function call, passing the app argument. 
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'))
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
+
+
+
+
