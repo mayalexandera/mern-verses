67cb67382810ddde03e655c010455c4562b5df54
mayalexandera <maya.blevins@gmail.com>
2020-12-04 18:05:53 -0500

    change relative path to absolute for passport google strategy config

--- services/passport.js
+++ services/passport.js
@@ -30,8 +30,9 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
-      proxy: true
+      callbackURL:
+        "https://boiling-beyond-29110.herokuapp.com/auth/google/callback",
+      proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then((existingUser) => {
