b20e3ca636e22a6defebb4bd290602a8ba0ef7d8
mayalexandera <maya.blevins@gmail.com>
2021-01-05 17:31:30 -0500

    reset config vars for database

--- routes/sizeRoutes.js
+++ routes/sizeRoutes.js
@@ -3,7 +3,6 @@ const Sizes = mongoose.model("sizes");
 
 module.exports = (app) => {
   app.get("/api/sizes", async (req, res) => {
-    console.log(req.query)
     const sizes = await Sizes.find({product: req.query.product});
     console.log(sizes);
     res.send(sizes);
