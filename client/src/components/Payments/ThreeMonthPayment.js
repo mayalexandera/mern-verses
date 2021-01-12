cfa0d8366fc1a12b013062a06f973028c33d423d
mayalexandera <maya.blevins@gmail.com>
2021-01-12 15:14:20 -0500

    formatting numbers

--- client/src/components/Payments/ThreeMonthPayment.js
+++ client/src/components/Payments/ThreeMonthPayment.js
@@ -7,7 +7,7 @@ const ThreeMonthPayment = ({amt, credits, terms, handleThreeMonthToken, id}) => 
     return (
       <StripeCheckout
         name='VERSES'
-        description={`$${amt*credits} ($${amt} ${terms})`}
+        description={`$${Number(amt*credits)} ($${amt} ${terms})`}
         amount={Number(amt*credits*100)}
         token={(token) => handleThreeMonthToken(token, amt, credits)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
