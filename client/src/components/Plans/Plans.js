b0c5914bf2ec2edf4609037c413d5263c48c202b
mayalexandera <maya.blevins@gmail.com>
2021-01-15 13:35:57 -0500

    connect plans to redux

--- client/src/components/Plans/Plans.js
+++ client/src/components/Plans/Plans.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from 'react-redux'
 import * as actions from "../../store/actions";
 import PlanCard from './PlanCard'
 import './Plans.css'
 
 
-const Plans = ( props ) => {
-  console.log(props)
+const Plans = ( { fetchPlans, plans } ) => {
+  useEffect(() => {
+   fetchPlans()
+  }, [])
 
   const renderPlans = () => {
-    return props.plans.map(plan => {
+    return plans.map(plan => {
       return (
-        <PlanCard plan={plan} ></PlanCard>
+        <PlanCard key={plan._id} plan={plan} ></PlanCard>
       )
     })
   }
@@ -19,7 +21,7 @@ const Plans = ( props ) => {
     <div className='plans-container'>
       <h2 style={{ textAlign: "center" }}>Plans</h2>
       <div className='plans-section'>
-        {props.plans ? renderPlans() : null}</div>
+        {plans ? renderPlans() : null}</div>
     </div>
   );
 }
