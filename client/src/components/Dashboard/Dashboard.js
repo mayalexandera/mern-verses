ccfe4a1addf0773f8ed23e2c2b46d3f99220a22b
mayalexandera <maya.blevins@gmail.com>
2021-02-04 12:15:49 -0500

    sizing profile pages

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -3,24 +3,29 @@ import { NavLink } from "react-router-dom";
 import "./Dashboard.css";
 
 const Dashboard = (props) => {
-
-  console.log(props)
+  console.log(props);
 
   return (
-   <div className='dashboard-wrapper-container'>
+    <div className='dashboard-wrapper-container'>
       <div className='dashboard-container'>
-        <div className='dashboard-header'><div className='dashboard-header-title'>{props.title || '...loading'}</div></div>
-        <div className='dashboard-wrapper'>
-          <ul className='dashboard-list'>
-            <NavLink to='/member/profile'>Profile</NavLink>
-            <NavLink to='/orders'>Orders</NavLink>
-            <NavLink to='/favorites'>Favorites</NavLink>
-            <NavLink to='/settings'>Account</NavLink>
-          </ul>
+        <div className='dashboard-header-wrapper'>
+          <div className='dashboard-header'>
+            <div className='dashboard-header-title'>
+              {props.title || "...loading"}
+            </div>
+          </div>
+          <div className='dashboard-wrapper'>
+            <ul className='dashboard-list'>
+              <NavLink to='/member/profile'>Profile</NavLink>
+              <NavLink to='/orders'>Orders</NavLink>
+              <NavLink to='/favorites'>Favorites</NavLink>
+              <NavLink to='/settings'>Account</NavLink>
+            </ul>
+          </div>
         </div>
       </div>
-   </div>
+    </div>
   );
-}
+};
 
 export default Dashboard;
