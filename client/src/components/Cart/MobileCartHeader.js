4b12099e349692f52f5f261d78a13e32e9886617
mayalexandera <maya.blevins@gmail.com>
2021-02-16 00:21:40 -0500

    add cart state to mobile cart header

--- client/src/components/Cart/MobileCartHeader.js
+++ client/src/components/Cart/MobileCartHeader.js
@@ -1,18 +1,24 @@
 import React from "react";
 import "./MobileCartHeader.css";
+import { connect } from 'react-redux'
 
-const MobileCartHeader = (props) => {
+const MobileCartHeader = ({ cart }) => {
   return (
     <div className='mobile-cart-container'>
       <div className='mobile-cart-header'>
         <div className='col-sm-12 mobile-title'>Bag</div>
         <div className='col-sm-12 mobile-subtitle'>
-          {"5 Items  | "}
-          <span className='mobile-cart-total'>$286.56</span>
+          {cart.items.length === 1 ? `${cart.items.length} Item` :`${cart.items.length} Items` || '0 Items' }
+          <span className='separator'>|</span>
+          <span className='mobile-cart-total'>{`$${cart.totals.subTotal}.00` || '$0.00'}</span>
         </div>
       </div>
     </div>
   );
 };
 
-export default MobileCartHeader;
+const mapStateToProps = ({ cart }) => {
+  return { cart }
+}
+
+export default connect(mapStateToProps)(MobileCartHeader);
