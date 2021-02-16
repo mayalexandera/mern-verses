2f79e5e400600bc8d361c8a2312bdf642469803d
mayalexandera <maya.blevins@gmail.com>
2021-02-16 17:02:58 -0500

    z-index

--- client/src/components/Dropdown/GreetingButton.js
+++ client/src/components/Dropdown/GreetingButton.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 
 const GreetingButton = ({ showAccount, user }) => {
-
   const handleClick = (e) => {
-    console.log(user)
-    showAccount()
-  }
+    console.log(user);
+    showAccount();
+  };
   return (
     <div onClick={(e) => handleClick(e)} className='greeting-button-wrapper'>
       <button className='greeting-button'>
@@ -18,7 +17,7 @@ const GreetingButton = ({ showAccount, user }) => {
         >
           <path d='M16.44 11A5.94 5.94 0 0 0 18 7 6 6 0 0 0 6 7a5.94 5.94 0 0 0 1.56 4A5 5 0 0 0 3 16v5a1 1 0 0 0 2 0v-5a3 3 0 0 1 3-3h8a3 3 0 0 1 3 3v5a1 1 0 0 0 2 0v-5a5 5 0 0 0-4.56-5zM8 7a4 4 0 1 1 4 4 4 4 0 0 1-4-4z'></path>
         </svg>
-        <p>Hi, {user && user.displayName}</p>
+        <p>{`Hi, ${user && user.displayName}`}</p>
       </button>
       <span className='material-icons-round'>navigate_next</span>
     </div>
@@ -26,7 +25,7 @@ const GreetingButton = ({ showAccount, user }) => {
 };
 
 const mapStateToProps = ({ auth: { user } }) => {
-  return { user }
-}
+  return { user };
+};
 
 export default connect(mapStateToProps)(GreetingButton);
